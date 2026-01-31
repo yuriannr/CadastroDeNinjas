33a3aff47e5db34c2baace833193068d3ce8348b
yuriannr <yuriaraujonr@gmail.com>
2026-01-31 17:30:28 -0300

    Criado a Controller

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Controlador para API Rest
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas") //Pegar Informações
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
