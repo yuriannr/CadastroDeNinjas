b9e145729846f5e780014169fae4d7bba36922cb
yuriannr <yuriaraujonr@gmail.com>
2026-02-20 13:59:54 -0300

    Atualiza os controllers com mock de dados

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,27 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class MissoesController {
+
+    @PostMapping("/criarmissao")
+    public String criarMissao(){
+        return "Criar uma nova missão";
+    }
+
+    @GetMapping("/mostrarmissão")
+    public String mostrarMissão(){
+        return "Mostrar missão";
+    }
+    @PutMapping("/alterarmissao")
+    public String alterarMissao(){
+        return "Alterar missão";
+    }
+    @DeleteMapping("/deletarmissao")
+    public String deletarMissao(){
+        return "Alterar Missão";
+    }
+
 }
--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //Controlador para API Rest
 @RequestMapping
@@ -11,5 +9,34 @@ public class NinjaController {
     @GetMapping("/boasvindas") //Pegar Informações
     public String boasVindas(){
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    //Adicionar novo Ninja(CREATE)
+    @PostMapping("/add")
+    public String add(){
+        return "Adicionar ninja";
+    }
+
+    //Mostrar todos os Ninjas (READ)
+    @GetMapping("/all")
+    public String mostrarTodos(){
+        return "Mostrar todos ninjas";
+    }
+    //Mostrar Ninjas por ID (READ)
+    @GetMapping("/ninjasporid")
+    public String ninjasPorID(){
+        return "Mostrar Ninjas por ID";
+    }
+
+    //Alterar Ninjas (UPDATE)
+    @PutMapping("/alterarninjaporid")
+    public String alterarPorID(){
+        return "Alterar ninja por ID";
+    }
+
+    //Deletar um Ninjas (DELETE)
+    @DeleteMapping("/deletarnijaporid")
+    public String deleteNinjaPorID(){
+        return "Deletar Ninja por ID";
     }
 }
