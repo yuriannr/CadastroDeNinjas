e2a929a2db73c81d0a0cb1cd42a884fcd1559574
yuriannr <yuriaraujonr@gmail.com>
2026-02-21 19:12:14 -0300

    feat: Adiciona o metodo listarNinjas(), para mostara uma requisição em uma rota direto do DB, no formato JSON

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,17 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
 public class NinjaService {
+
+    private final NinjaRepository ninjaRepository; //injeção de dependencia Respository
+
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
 }
