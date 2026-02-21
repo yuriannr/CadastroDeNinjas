d76322852b13705cc4de769094ac389570592396
yuriannr <yuriaraujonr@gmail.com>
2026-02-21 19:14:51 -0300

    inserre o @JsonIgnore para solucionar o erro de loop de serialização

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -29,5 +30,6 @@ public class MissoesModel {
     private String imgUrl;
     //Uma missão para muitos ninjas
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 }
