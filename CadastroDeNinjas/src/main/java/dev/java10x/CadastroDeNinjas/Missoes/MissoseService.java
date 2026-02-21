dbd067a45105cc672c95385132daff7c8c238f83
yuriannr <yuriaraujonr@gmail.com>
2026-02-21 19:22:12 -0300

    fix: apenas faz a injeção de dependencia ninjaRepository

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoseService.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoseService.java
@@ -1,4 +1,11 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MissoseService {
+
+    private final MissoesRepository missoesRepository;
 }
