4fc0f2d417442b2b623609d955431111c1a2791a
yuriannr <yuriaraujonr@gmail.com>
2026-02-22 12:22:43 -0300

    feat: adiciona o FindAll(), para as missões

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,19 +1,25 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/missoes")
+@RequiredArgsConstructor
 public class MissoesController {
+
+    private final MissoseService missoesService;
+
 
     @PostMapping("/criar")
     public String criarMissao(){
         return "Criar uma nova missão";
     }
 
-    @GetMapping("/mostrar")
-    public String mostrarMissão(){
-        return "Mostrar missão";
+    @GetMapping("/listar")
+    public List <MissoesModel> litarMissoes(){
+        return missoesService.listarMissoes();
     }
     @PutMapping("/alterar")
     public String alterarMissao(){
